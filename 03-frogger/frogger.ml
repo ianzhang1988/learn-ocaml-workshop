a55d808dde0f09e47e20b8627575f3750f3b3518
ian.zhang.88 <ian.zhang.88@outlook.com>
2026-08-31 00:08:55 +0800

    froger + one car movement

--- 03-frogger/frogger.ml
+++ 03-frogger/frogger.ml
@@ -8,32 +8,76 @@ open Scaffold
 
 module Frog = struct
   type t =
-    { position : Position.t
+    { position : Position.t;
+      img      : Image.t
     } [@@deriving fields]
 
   let create = Fields.create
 
-  let up    f = {position = { f.position with y = (f.position.y + 1) } }
-  let down  f = {position = { f.position with y = (f.position.y - 1) } }
-  let left  f = {position = { f.position with x = (f.position.x - 1) } }
-  let right f = {position = { f.position with x = (f.position.x + 1) } }
+  let up    f = {position = { f.position with y = (f.position.y + 1) }; img = Image.Frog_up }
+  let down  f = {position = { f.position with y = (f.position.y - 1) }; img = Image.Frog_down }
+  let left  f = {position = { f.position with x = (f.position.x - 1) }; img = Image.Frog_left }
+  let right f = {position = { f.position with x = (f.position.x + 1) }; img = Image.Frog_right }
+
+end
+
+let in_canvas_fun x1 x2 y1 y2 =
+  fun (pos:Position.t) ->
+    let x_in_range = pos.x >= x1 && pos.x < x2 in
+    let y_in_range = pos.y >= y1 && pos.y < y2 in
+    x_in_range && y_in_range
+
+let in_canvas =
+  in_canvas_fun 0 Board.num_cols 0 (List.length Board.rows) 
+
+module Non_frog_character = struct
+  module Kind = struct
+    type t =
+      | Car
+      | Log
+  end
+
+  type t =
+    {
+      kind: Kind.t;
+      pos:  Position.t;
+      speed: int;
+      shadows: bool list;
+    } [@@deriving fields]
+
+
+  let kind t = t.kind
+  let position t = t.pos
+
+  (** In units of grid-points/tick. Positive values indicate rightward motion,
+     negative values leftward motion. *)
+  let horizontal_speed t = t.speed
+  let shadows t = t.shadows
+
+  let img t =
+    match t.kind with
+    | Car -> Image.Car1_right
+    | Log -> Image.Car1_right
 
 end
 
 module World = struct
   type t =
-    { frog  : Frog.t
+    { frog  : Frog.t;
+      nfcs   : Non_frog_character.t list
     } [@@deriving fields]
 
   let create = Fields.create
 end
 
 let create_frog () =
-  Frog.create ~position:(Position.create ~x:5 ~y:1)
+  Frog.create ~position:(Position.create ~x:5 ~y:0) ~img:Image.Frog_up
 ;;
 
 let create () =
-  World.create ~frog:(create_frog () )
+  let shadows = List.init 10 ~f:(fun _ -> Random.bool () ) in
+  let nfc:Non_frog_character.t = {kind = Car; pos = {x = 0; y = 1}; speed = 1; shadows = shadows} in
+  World.create ~frog:(create_frog () ) ~nfcs:[nfc]
 ;;
 
 let tick (w : World.t) =
@@ -44,7 +88,17 @@ let tick (w : World.t) =
   (*    you can just return [world] here. Later you'll want do interesting things \ *)
   (*    like move all the cars and logs, detect collisions and figure out if the \ *)
   (*    player has died or won. " *)
-  w
+
+  let new_nfcs =
+    List.map w.nfcs ~f:(
+      fun nfc ->
+        {nfc with pos = { nfc.pos with x = (nfc.pos.x + nfc.speed) % Board.num_cols;  }}
+      )
+  in
+  {
+    w with
+    nfcs = new_nfcs
+  }
 ;;
 
 let handle_input (w : World.t) (k : Key.t) =
@@ -57,26 +111,38 @@ let handle_input (w : World.t) (k : Key.t) =
 (*     { world with frog = Frog.create ... } *)
 (* " *)
   let new_frog = 
-  match k with
-  | Arrow_up -> Frog.up w.frog
-  | Arrow_down -> w.frog
-  | Arrow_left -> w.frog
-  | Arrow_right -> w.frog in
-  World.create ~frog:new_frog
+    let frog_tmp = 
+      match k with
+      | Arrow_up -> Frog.up w.frog
+      | Arrow_down -> Frog.down w.frog
+      | Arrow_left -> Frog.left w.frog
+      | Arrow_right -> Frog.right w.frog in
+    if in_canvas frog_tmp.position then
+      frog_tmp
+    else
+      w.frog
+  in
+  (* World.create ~frog:new_frog *)
+  { w with frog = new_frog }
 ;;
 
 let draw (w : World.t) =
   (* failwith *)
   (*   "Return a list with a single item: a tuple consisting of one of the choices \ *)
   (*    in [Images.t] in [scaffold.mli]; and the current position of the [Frog]." *)
-  [(Image.Frog_up, w.frog.position)]
+  List.concat [
+    [(w.frog.img, w.frog.position)];
+    List.map w.nfcs ~f:(
+        fun nfc -> (Non_frog_character.img nfc, Non_frog_character.position nfc)
+    );
+  ]
 ;;
 
 let handle_event (w : World.t) (e : Event.t) =
   (* failwith *)
   (*   "This function should probably be just 3 lines long: [match event with ...]" *)
   match e with
-  | Tick -> w
+  | Tick -> tick w
   | Keypress k -> handle_input w k
 ;;
 
--- 03-frogger/frogger.mli
+++ 03-frogger/frogger.mli
@@ -1,8 +1,26 @@
 open Scaffold
 
+module Non_frog_character : sig
+  module Kind : sig
+    type t =
+      | Car
+      | Log
+  end
+
+  type t
+
+  val kind : t -> Kind.t
+  val position : t -> Position.t
+
+  (** In units of grid-points/tick. Positive values indicate rightward motion,
+     negative values leftward motion. *)
+  val horizontal_speed : t -> int
+end
+
 module World : sig
   type t
 end
+
 
 val create : unit -> World.t
 val tick : World.t -> World.t
